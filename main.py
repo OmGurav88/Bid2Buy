8109b143ea6a232b7746856194578e36d492e8c4
ingale-patil <sherbhau01@gmail.com>
2023-04-02 17:57:14 +0530

    Done By prayog
    Added highest Bid feature, all admin page, edit profile and all

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask_login import UserMixin,login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import login_user,logout_user,login_manager,LoginManager
 from flask_login import login_required,current_user
+import time
 from datetime import datetime
 import json 
 
@@ -87,6 +88,8 @@ class Users(db.Model):
     umail=db.Column(db.String(20), nullable = False)
     upass=db.Column(db.String(20),nullable = False)
     ucnfpass=db.Column(db.String(20), nullable = False)
+    ucity=db.Column(db.String(20), nullable = False)
+
 
 
 # class Admins(db.Model):
@@ -218,23 +221,25 @@ def signup():
         uname_db = request.form.get('user_name')
         unumber_db = request.form.get('user_phone')
         umail_db = request.form.get('user_email')
+        ucity_db = request.form.get('user_city')
         upass_db = request.form.get('password')
         ucnfpass_db = request.form.get('cnfPassword')
 
-        entry = Users( uname = uname_db , unumber = unumber_db , umail = umail_db , upass = upass_db , ucnfpass = ucnfpass_db )
+        entry = Users( uname = uname_db , unumber = unumber_db , umail = umail_db , upass = upass_db , ucnfpass = ucnfpass_db , ucity = ucity_db)
 
         # Check if the email already exists in the database
         # Query the database for a user with a specific email address
         user = Users.query.filter_by(umail=umail_db).first()
         print(user)
         if user:
-            # flash('Email already exists in the database')
+            flash('Email already exists in the database')
             print("user exists")
             return render_template('signup.html')
         else :
             db.session.add(entry)
             db.session.commit()
-            return render_template('login.html')
+            flash("Signup Success Please Login","success")
+            return render_template('signup.html')
 
 
     return render_template('signup.html')
@@ -247,7 +252,12 @@ def wologin():
 #Admin related stuff
 @app.route('/adminhome')
 def adminhome():
-    return render_template('adminhome.html')
+    total_users = Users.query.count()
+    total_products = Products.query.count()
+    total_bidders = Bidders.query.count()
+    total_queries = Contacts.query.count()
+    return render_template('adminhome.html', total_users=total_users, total_products=total_products, bidders=total_bidders, queries = total_queries)
+
 
 
 @app.route('/AdminLogin', methods=['GET', 'POST'])
@@ -288,25 +298,32 @@ def show_product(pid):
     #current_userId=session.get('id')
     # query=db.engine.execute(f"SELECT * FROM `products`;")
     user = db.session.execute(f"SELECT uid FROM `users` WHERE uname=:name;", {'name': current_user}).fetchone()
+    curr_bid = db.session.execute(f"SELECT max(bprice) FROM `bidders` where pid=:pid;", {'pid': pid}).fetchone()
     user_id = user[0]
+    print(curr_bid[0])
+    if(curr_bid[0] is None):
+        latest_bid = 0
+    else:
+        latest_bid = int(curr_bid[0])
+
     
     # print("/////////////////////////////////****************")
     # print(new_bid)
     if(request.method == 'POST' ):
         bprice_db = int(request.form.get('bid'))
-        base_price = db.session.execute(f"SELECT pprice FROM `products` WHERE pid=:pid;", {'pid': pid}).fetchone()
-        # unumber_db = request.form.get('user_phone')
-        base_price1 = int(base_price[0])
-        print(type(base_price1))
-        print(type(bprice_db))
         
-        if(base_price1 < bprice_db):
+        
+        if(latest_bid < bprice_db):
             pid_db=pid
             entry = Bidders( uid = user_id , bprice=bprice_db, pid=pid_db)
             db.session.add(entry)
             db.session.commit()
     
-    return render_template('product.html', product=product,id = user_id)
+    return render_template('product.html', product=product,id = user_id,curr_bid=latest_bid)
+
+@app.route('/sorry')
+def sorry():
+    render_template('sorry.html')
 
 
 @app.route('/myproduct/<int:uid>')
@@ -315,19 +332,24 @@ def my_products(uid):
     print("Yes")
     # product = Products.query.get('pid')
     my_product = db.session.execute(f"SELECT * FROM `bidders` WHERE uid=:uid;", {'uid': uid})
-    if my_product is None:
-        return 'Product not found', 404
+    #my_product1 = db.session.execute(f"SELECT pid FROM `bidders` WHERE uid=:uid;", {'uid': uid}).fetchone()
+    #print(my_product1[0])
+
+    
+    
     
     return render_template('myproduct.html', bidders=my_product)
     
 
 @app.route('/myprofile/<int:uid>')
 def my_profile(uid):
-    print("\nYes",uid)
+    #print("\nYes",uid)
     my_Profile = db.session.execute(f"SELECT * FROM `users` WHERE uid=:uid;", {'uid': uid}).fetchone()
     if my_Profile is None:
         print("Hello")
         return 'Profile not found', 404
+    
+    print(my_Profile.uname)
         
     
     return render_template('myprofile.html',profile=my_Profile)
@@ -344,13 +366,65 @@ def edit_profile(uid):
         new_email = request.form.get('email')
         new_phone = request.form.get('phone')
 
+
         db.session.execute('UPDATE `users` SET uname = :name, umail = :email, unumber = :phone WHERE uid = :id', {'name': new_name, 'email': new_email, 'phone': new_phone, 'id': uid})
         db.session.commit()
+
+        flash('Your Details Updated Successfully','success')
+        # return redirect(url_for('edit_profile', uid=uid))
+        return redirect(url_for('my_profile', uid=uid))
+        #Above is the function Bro.
         
 
     return render_template('editprofile.html',uid=uid)
 
 
+@app.route('/admin/users')
+def adminusers():
+    admin_users = db.engine.execute(f"SELECT * FROM `users`;")
+    return render_template('users-admin.html', users=admin_users)
+
+@app.route('/admin/edituser/<int:uid>', methods = [ 'GET' , 'POST'])
+def edit_user(uid): 
+    if(request.method == 'POST'):
+        # Add Entry TO Database
+        # contact_no , name, email, pNumber, message, dt 
+        # the first name is entry in the database and another name is for Html page 
+        new_name  = request.form.get('username')
+        new_email = request.form.get('email')
+        new_phone = request.form.get('mobile')
+        new_city = request.form.get('city')
+
+
+
+        db.session.execute('UPDATE `users` SET uname = :name, umail = :email, unumber = :phone, ucity = :city WHERE uid = :id', {'name': new_name, 'email': new_email, 'phone': new_phone, 'id': uid,'city':new_city })
+        db.session.commit()
+
+        flash('Your Details Updated Successfully','success')
+        # return redirect(url_for('edit_profile', uid=uid))
+        # return redirect(url_for('my_profile', uid=uid))
+        #Above is the function Bro.
+        
+
+    return render_template('edituser.html',uid=uid)
+
+
+@app.route('/admin/products')
+def adminproducts():
+    admin_products = db.engine.execute(f"SELECT * FROM `products`;")
+    return render_template('product-admin.html', products=admin_products)
+
+
+@app.route('/admin/bidders')
+def adminbidders():
+    admin_bidders = db.engine.execute(f"SELECT * FROM `bidders`;")
+    return render_template('bidders-admin.html', bidders=admin_bidders)
+
+
+@app.route('/admin/contacts')
+def admincontacts():
+    admin_contacts = db.engine.execute(f"SELECT * FROM `contacts`;")
+    return render_template('contact-admin.html', contacts=admin_contacts)
 
 
 if __name__ == "__main__":
